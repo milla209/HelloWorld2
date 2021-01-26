6159bbc0d334ccd6c357384ab096277b0ef8bcbf
Mila <milla209@mail.ru>
2021-01-26 11:52:15 +0300

    Initial Commit

---
+++ HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by Людмила Солопенко on 26.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
